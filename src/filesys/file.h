23c1cb664e0fd4628bfbbc55ac713f6bc651b0de
rdspring <rdspring1@gmail.com>
2013-05-03 01:29:41 -0500

    Added README comments to the modified source files

--- src/filesys/file.h
+++ src/filesys/file.h
@@ -1,3 +1,28 @@
+// Turnin Date: 5/3/2013
+//Name: Ryan Spring
+//EID: rds2367
+//CS login: rdspring
+//Email: rdspring1@gmail.com
+//Unique Number: 53426
+
+//Name1: Ruben Fitch
+//EID1: rsf293
+//CS login: rubensam
+//Email: rubensamf@utexas.edu
+//Unique Number: 53435
+
+//Name2: Josue Roman
+//EID2: jgr397
+//CS login: jroman
+//Email: jozue.roman@gmail.com
+//Unique Number: 53425
+
+//Name3: Peter Farago
+//EID2: pf3546
+//CS login: farago
+//Email: faragopeti@tx.rr.com
+//Unique Number: 53435
+
 #ifndef FILESYS_FILE_H
 #define FILESYS_FILE_H
 
@@ -10,7 +35,7 @@ struct inode;
 struct file 
 {
 	struct inode *inode;        /* File's inode. */
-	struct dir *dir;			/* The dir struct if the file is a director */
+	struct dir *dir;			/* The dir struct if the file is a directory */
 	off_t pos;                  /* Current position. */
 	bool deny_write;            /* Has file_deny_write() been called? */
 };
--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -1,3 +1,28 @@
+// Turnin Date: 5/3/2013
+//Name: Ryan Spring
+//EID: rds2367
+//CS login: rdspring
+//Email: rdspring1@gmail.com
+//Unique Number: 53426
+
+//Name1: Ruben Fitch
+//EID1: rsf293
+//CS login: rubensam
+//Email: rubensamf@utexas.edu
+//Unique Number: 53435
+
+//Name2: Josue Roman
+//EID2: jgr397
+//CS login: jroman
+//Email: jozue.roman@gmail.com
+//Unique Number: 53425
+
+//Name3: Peter Farago
+//EID2: pf3546
+//CS login: farago
+//Email: faragopeti@tx.rr.com
+//Unique Number: 53435
+
 #include "userprog/exception.h"
 #include <debug.h>
 #include <inttypes.h>
